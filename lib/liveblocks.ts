d04a26ccdaa6559929cf03b8a0ac4b76917530cf
shahin-r71 <shahinlv01@gmail.com>
2024-12-09 16:14:01 +0600

    liveblocks secret edited

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,6 +1,6 @@
 import { Liveblocks } from "@liveblocks/node";
 
 const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_SECRET!,
+  secret: process.env.LIVEBLOCKS_SECRET as string,
 });
 export {liveblocks};
